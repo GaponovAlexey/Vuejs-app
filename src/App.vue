b84927832b25cd4af58ec0413b703d8478a12eac
kekelberi <alexei.gaponov@gmail.com>
2023-02-13 20:32:30 -0600

    save

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>
+    <div>
+      <button v-on:click="addLike" >Like</button>
+      <button @click="addDislike" >Dislike</button>
+    </div>
+    <div>
+      Количество лайков: <strong>{{ likes }}</strong>
+    </div>
+    <div>
+      Количество дизлайков: <strong>{{ dislike }}</strong>
+    </div>
+  </h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      likes: 0,
+      dislike: 5,
+    };
+  },
+  methods: {
+    addLike() {
+      this.likes += 1
+      this.dislike -= 1
+    },
+    addDislike() {
+      this.dislike += 1
+      this.likes -= 1  
+    }
+  }
+};
+</script>
+
+<style></style>
