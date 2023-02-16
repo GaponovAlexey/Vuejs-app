3961ac711c84e9c3d4f1843c56a84721ae69409b
kekelberi <alexei.gaponov@gmail.com>
2023-02-15 20:35:12 -0600

    save

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,72 @@
-<!-- <template>
-  <h1>
-    <Add/>
-    <div>
-      <button v-on:click="addLike" >Like</button>
-      <button @click="addDislike" >Dislike</button>
+<template>
+  <div class="app">
+    <form class="form">
+      <input class="input" type="text" placeholder="name" />
+      <input class="input" type="text" placeholder="desc" />
+      <button class="btn" >send</button>
+    </form>
+    <div class="post" v-for="post in post">
+      <div><strong>name:</strong>{{ post.title }}</div>
+      <div><strong>desc:</strong>{{ post.body }}</div>
     </div>
-    <div>
-      Количество лайков: <strong>{{ likes }}</strong>
-    </div>
-    <div>
-      Количество дизлайков: <strong>{{ dislike }}</strong>
-    </div>
-  </h1>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      likes: 0,
-      dislike: 5,
+      post: [
+        { id: 1, title: "javascript", body: "describe1" },
+        { id: 2, title: "Go", body: "describe2" },
+        { id: 3, title: "Rast", body: "describe3" },
+        { id: 4, title: "Air", body: "describe3" },
+      ],
     };
   },
   methods: {
     addLike() {
-      this.likes += 1
-      this.dislike -= 1
+      this.likes += 1;
+      this.dislike -= 1;
     },
     addDislike() {
-      this.dislike += 1
-      this.likes -= 1  
-    }
-  }
-};
-</script>
-
-<style></style> -->
-
-<template>
-  <h1>hi</h1>
-  <div>one {{ count }}</div>
-  <div>two</div>
-  <button @:click="add" >+</button>
-</template>
-
-<script>
-export default {
-  data() {
-    return {
-      count: 0,
-    };
+      this.dislike += 1;
+      this.likes -= 1;
+    },
   },
-  methods: {
-    add() {
-      this.count ++
-    }
-  }
 };
 </script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
+}
+.post {
+  padding: 15px;
+  border: 1px solid teal;
+  margin-top: 15px;
+}
+.form {
+  display: flex;
+  flex-direction: col;
+}
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal ;
+}
+.input {
+  width: 100%;
+  border: 1px solid orange;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+</style>
