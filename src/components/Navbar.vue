fd835b5e099fc954bb64b69aab1906d843884f98
kekelberi <alexei.gaponov@gmail.com>
2023-02-27 18:44:07 -0600

    save

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,11 +2,11 @@
   <div class="navbar">
     <div style="cursor: pointer" @click="$router.push('/')">Vue 3</div>
     <div>
-      <my-button @click="$router.push('/posts')">Posts Local</my-button>
-      <my-button @click="$router.push('/store')" style="margin-left: 20px"
+      <my-button class="button"  @click="$router.push('/posts')">Posts Local</my-button>
+      <my-button class="button"  @click="$router.push('/store')" style="margin-left: 20px"
         >Posts Store vuex</my-button
       >
-      <my-button @click="$router.push('/composition')" style="margin-left: 20px"
+      <my-button class="button"  @click="$router.push('/composition')" style="margin-left: 20px"
         >Composition API: Lifecycle Hooks</my-button
       >
     </div>
@@ -27,5 +27,8 @@ export default {
   display: flex;
   align-items: center;
   padding: 0 15px;
+}
+.button {
+  color: black;
 }
 </style>
